3776d72de0f6892fbb5784a91edb8f7493ece7d7
Abiodun Allison <allisonabiodun@gmail.com>
2024-02-08 17:20:54 +0100

    feat: combined build index and query engine for different rag types

--- backend.py
+++ backend.py
@@ -1,21 +1,34 @@
-import warnings
-from scripts import utils
+from typing import List, Literal
 
-warnings.filterwarnings("ignore")
+from scripts.query_engine_builder import build_index, get_query_engine
 
-import openai
-import nest_asyncio
-from llama_index.llms import OpenAI
-
-from scripts.basic_rag import build_basic_rag_index, get_basic_rag_query_engine
-from scripts.sentence_window import build_sentence_window_index, get_sentence_window_query_engine
-from scripts.auto_merging import build_automerging_index, get_automerging_query_engine
-
-nest_asyncio.apply() 
-
-openai.api_key = utils.get_openai_api_key()
-
-model_name = "gpt-3.5-turbo"
-llm = OpenAI(model=model_name, temperature=0.1)
+from llama_index import Document
 
 
+def build_index_get_query_engine(
+    documents: List[Document],
+    llm,
+    embed_model: str = "local:BAAI/bge-small-en-v1.5",
+    window_size: int = 3,
+    chunk_sizes: List[int] | None = None,
+    rag_type: Literal["basic", "sentence_window", "auto_merging"] = "basic",
+    similarity_top_k: int = 6,
+    rerank_top_n: int = 2,
+):
+    index = build_index(
+        documents=documents,
+        llm=llm,
+        embed_model=embed_model,
+        window_size=window_size,
+        chunk_sizes=chunk_sizes,
+        rag_type=rag_type,
+    )
+
+    query_engine = get_query_engine(
+        index=index,
+        similarity_top_k=similarity_top_k,
+        rerank_top_n=rerank_top_n,
+        rag_type=rag_type,
+    )
+
+    return query_engine
--- test.py
+++ test.py
@@ -1,20 +1,12 @@
-from pprint import pprint
 import warnings
 
 warnings.filterwarnings("ignore")
 
-from llama_index import SimpleDirectoryReader
-from llama_index.llms import OpenAI
-
 import openai
+from llama_index import SimpleDirectoryReader
 
 from scripts import utils
-from scripts.basic_rag import build_basic_rag_index, get_basic_rag_query_engine
-from scripts.sentence_window import (
-    build_sentence_window_index,
-    get_sentence_window_query_engine,
-)
-from scripts.auto_merging import build_automerging_index, get_automerging_query_engine
+from backend import build_index
 
 
 openai.api_key = utils.get_openai_api_key()
@@ -23,27 +15,6 @@ documents = SimpleDirectoryReader(
     input_files=["pdfs/eBook-How-to-Build-a-Career-in-AI.pdf"]
 ).load_data()
 
+index = build_index(documents, rag_type="veronica")
 
-llm = OpenAI(model="gpt-3.5-turbo", temperature=0.1)
-
-
-# Basic RAG
-basic_index = build_basic_rag_index(documents, llm)
-query_engine = get_basic_rag_query_engine(basic_index)
-
-response = query_engine.query("What is the importance of networking in AI?")
-
-response.print_response_stream()
-# pprint(response.print_response_stream())
-
-# Sentence Window
-# sentence_index = build_sentence_window_index(document, llm)
-# query_engine = get_sentence_window_query_engine(sentence_index)
-
-# pprint(query_engine.query("What is the importance of networking in AI?").response)
-
-# Auto Merging
-# automerging_index = build_automerging_index(documents, llm)
-# query_engine = get_automerging_query_engine(automerging_index)
-
-# pprint(query_engine.query("What is the importance of networking in AI?").response)
+print(index)
