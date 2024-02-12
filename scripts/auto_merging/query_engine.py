eb924f56e2e24f4809a98123d80a098311d1c218
Abiodun Allison <allisonabiodun@gmail.com>
2024-02-12 18:14:25 +0100

    chore: merged rag types from main

---
+++ scripts/auto_merging/query_engine.py
@@ -0,0 +1,28 @@
+from llama_index import (
+    VectorStoreIndex,
+)
+from llama_index.indices.postprocessor import SentenceTransformerRerank
+from llama_index.retrievers import AutoMergingRetriever
+from llama_index.query_engine import RetrieverQueryEngine
+from llama_index.indices.base import BaseIndex
+
+
+def get_automerging_query_engine(
+    index: VectorStoreIndex | BaseIndex,
+    similarity_top_k=12,
+    rerank_top_n=2,
+) -> RetrieverQueryEngine:
+    base_retriever = index.as_retriever(
+        similarity_top_k=similarity_top_k, streaming=True
+    )
+    retriever = AutoMergingRetriever(
+        base_retriever, index.storage_context, verbose=True
+    )
+    rerank = SentenceTransformerRerank(
+        top_n=rerank_top_n, model="BAAI/bge-reranker-base"
+    )
+    auto_merging_engine = RetrieverQueryEngine.from_args(
+        retriever, node_postprocessors=[rerank], streaming=True
+    )
+
+    return auto_merging_engine
