1876a774a79694070a6eb9d86ad426d3e684cc02
Abiodun Allison <allisonabiodun@gmail.com>
2024-02-13 17:29:09 +0100

    chore: update llm model name

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import warnings
 
 from llama_index import SimpleDirectoryReader
 from llama_index.query_engine import BaseQueryEngine, RetrieverQueryEngine
+from llama_index.chat_engine.types import ChatMode
+from llama_index.memory import ChatMemoryBuffer
 
 from backend import build_index_and_query_engine
 from scripts.query_engine_builder import build_index
@@ -12,13 +14,22 @@ documents = SimpleDirectoryReader(
     input_files=["pdfs/eBook-How-to-Build-a-Career-in-AI.pdf"]
 ).load_data()
 
-index = build_index(documents, rag_type="auto_merging")
+index = build_index(documents, rag_type="basic")
 
-chat_engine = index.as_chat_engine()
+memory = ChatMemoryBuffer.from_defaults(token_limit=3900)
+chat_engine = index.as_chat_engine(
+    chat_mode=ChatMode.CONDENSE_PLUS_CONTEXT,
+    verbose=True,
+    similiarity_top_k=3,
+    memory=memory,
+)
 
-response =  chat_engine.chat("What is the best way to build a career in AI?")
+for _ in range(2):
+    print("\n")
+    question = input("Ask me anything: ")
+    response = chat_engine.chat(question)
 
-print(response.response)
+    print(response.response)
 
 # query_engine: BaseQueryEngine | RetrieverQueryEngine = build_index_and_query_engine(
 #     documents, rag_type="auto_merging"
