bfaa1a0ca5f94c479f0608a5a35c209f7689543d
Abiodun Allison <allisonabiodun@gmail.com>
2024-02-20 12:13:25 +0100

    chore: use OOP for query engine builder

--- scripts/query_engine_builder.py
+++ scripts/query_engine_builder.py
@@ -12,14 +12,13 @@ from scripts.sentence_window.query_engine import get_sentence_window_query_engin
 from scripts.auto_merging.build_index import build_automerging_index
 from scripts.auto_merging.query_engine import get_automerging_query_engine
 
-import nest_asyncio
-import openai
-from llama_index.llms.openai import OpenAI
 from llama_index.core import Document, VectorStoreIndex
 from llama_index.core.indices.base import BaseIndex
 from llama_index.core.query_engine import RetrieverQueryEngine, BaseQueryEngine
 from llama_index.core.embeddings.utils import EmbedType
 
+
+import nest_asyncio
 
 nest_asyncio.apply()
 warnings.filterwarnings("ignore")
@@ -28,6 +27,7 @@ warnings.filterwarnings("ignore")
 class IndexParams(TypedDict):
     documents: List[Document]
     embed_model: EmbedType
+    save_dir: PathLike[str]
 
 
 class QueryParams(TypedDict):
@@ -35,60 +35,69 @@ class QueryParams(TypedDict):
     similarity_top_k: int
 
 
-def build_index(
-    documents: List[Document],
-    # save_dir: PathLike,
-    embed_model: EmbedType,
-    window_size: int = 3,
-    chunk_sizes: List[int] | None = None,
-    rag_type: Literal["basic", "sentence_window", "auto_merging"] = "basic",
-) -> VectorStoreIndex | BaseIndex:
-    index_params: IndexParams = {
-        "documents": documents,
-        "embed_model": embed_model,
-        # "save_dir": save_dir,
-    }
+class QueryEngineBuilder:
+    def __init__(
+        self,
+        documents: List[Document],
+        embed_model: EmbedType,
+        save_dir: PathLike[str],
+        rag_type: Literal["basic", "sentence_window", "auto_merging"] = "basic",
+    ):
+        self.embed_model = embed_model
+        self.documents = documents
+        self.rag_type = rag_type
+        self.save_dir = save_dir
 
-    index_builders = {
-        "basic": build_basic_rag_index(**index_params),
-        "sentence_window": build_sentence_window_index(
-            **index_params,
-            window_size=window_size,
-        ),
-        "auto_merging": build_automerging_index(
-            **index_params,
-            chunk_sizes=chunk_sizes,
-        ),
-    }
+        self.index_params: IndexParams = {
+            "documents": self.documents,
+            "embed_model": self.embed_model,
+            "save_dir": self.save_dir,
+        }
 
-    try:
-        return index_builders[rag_type]
-    except KeyError:
-        raise ValueError(f"Invalid rag_type: {rag_type}")
+    def build_index(
+        self,
+        window_size: int = 3,
+        chunk_sizes: List[int] | None = None,
+    ) -> VectorStoreIndex | BaseIndex:
+        index_builders = {
+            "basic": build_basic_rag_index(**self.index_params),
+            "sentence_window": build_sentence_window_index(
+                **self.index_params,
+                window_size=window_size,
+            ),
+            "auto_merging": build_automerging_index(
+                **self.index_params,
+                chunk_sizes=chunk_sizes,
+            ),
+        }
 
+        try:
+            return index_builders[self.rag_type]
+        except KeyError:
+            raise ValueError(f"Invalid rag_type: {self.rag_type}")
 
-def get_query_engine(
-    index: VectorStoreIndex | BaseIndex,
-    similarity_top_k: int = 6,
-    rerank_top_n: int = 2,
-    rag_type: Literal["basic", "sentence_window", "auto_merging"] = "basic",
-) -> BaseQueryEngine | RetrieverQueryEngine:
-    query_params: QueryParams = {
-        "index": index,
-        "similarity_top_k": similarity_top_k,
-    }
+    def get_query_engine(
+        self,
+        similarity_top_k: int = 6,
+        rerank_top_n: int = 2,
+    ) -> BaseQueryEngine | RetrieverQueryEngine:
 
-    query_engines = {
-        "basic": get_basic_rag_query_engine(**query_params),
-        "sentence_window": get_sentence_window_query_engine(
-            **query_params, rerank_top_n=rerank_top_n
-        ),
-        "auto_merging": get_automerging_query_engine(
-            **query_params, rerank_top_n=rerank_top_n
-        ),
-    }
+        query_params: QueryParams = {
+            "index": self.build_index(),
+            "similarity_top_k": similarity_top_k,
+        }
 
-    try:
-        return query_engines[rag_type]
-    except KeyError:
-        raise ValueError(f"Invalid rag_type: {rag_type}")
+        query_engines = {
+            "basic": get_basic_rag_query_engine(**query_params),
+            "sentence_window": get_sentence_window_query_engine(
+                **query_params, rerank_top_n=rerank_top_n
+            ),
+            "auto_merging": get_automerging_query_engine(
+                **query_params, rerank_top_n=rerank_top_n
+            ),
+        }
+
+        try:
+            return query_engines[self.rag_type]
+        except KeyError:
+            raise ValueError(f"Invalid rag_type: {self.rag_type}")
