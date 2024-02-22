583b732c16e3211dcc918370137dcd29a6b718e8
Abiodun Allison <allisonabiodun@gmail.com>
2024-02-22 16:39:21 +0100

    feat: use chat engine for sentence window

--- scripts/sentence_window/chat_engine.py
+++ scripts/sentence_window/chat_engine.py
@@ -1,25 +1,34 @@
+from scripts.utils import memory
+
 from llama_index.core.postprocessor import (
     MetadataReplacementPostProcessor,
     SentenceTransformerRerank,
 )
-from llama_index.core.query_engine import BaseQueryEngine
 from llama_index.core.indices.base import BaseIndex
+from llama_index.core.llms.utils import LLMType
+from llama_index.core.chat_engine.types import ChatMode, BaseChatEngine
 
 
 def build_sentence_window_chat_engine(
+    llm: LLMType,
     index: BaseIndex,
     similarity_top_k=6,
     rerank_top_n=2,
-) -> BaseQueryEngine:
+) -> BaseChatEngine:
     # define postprocessors
     postproc = MetadataReplacementPostProcessor(target_metadata_key="window")
     rerank = SentenceTransformerRerank(
         top_n=rerank_top_n, model="cross-encoder/ms-marco-TinyBERT-L-2-v2"
     )
 
-    sentence_window_engine = index.as_query_engine(
-        similarity_top_k=similarity_top_k,
+    sentence_window_engine = index.as_chat_engine(
+        llm=llm,
+        chat_mode=ChatMode.CONDENSE_PLUS_CONTEXT,
+        similiarity_top_k=similarity_top_k,
+        memory=memory,
+        verbose=True,
         node_postprocessors=[postproc, rerank],
         streaming=True,
     )
+
     return sentence_window_engine
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,9 +1,26 @@
 import os
+from os import PathLike
 from dotenv import load_dotenv, find_dotenv
+from typing import TypedDict, List
 
 from llama_index.core.memory import ChatMemoryBuffer
+from llama_index.core import Document
+from llama_index.core.indices.base import BaseIndex
+from llama_index.core.embeddings.utils import EmbedType
 
 memory = ChatMemoryBuffer.from_defaults(token_limit=3900)
+
+
+class IndexParams(TypedDict):
+    documents: List[Document]
+    embed_model: EmbedType
+    save_dir: PathLike[str]
+
+
+class QueryParams(TypedDict):
+    index: BaseIndex
+    similarity_top_k: int
+    llm: LLMType
 
 
 def get_openai_api_key():
